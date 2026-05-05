4011d59aca76d6d3dd7d08e908cbbbedec3ea4a2
kiaravzm <kiaramartins.design@gmail.com>
2026-05-04 21:27:02 -0300

    chore: configure vitest, testing library and jsdom

---
+++ src/test/setup.ts
@@ -0,0 +1,6 @@
+import '@testing-library/jest-dom'
+
+// aqui você também vai inicializar o MSW futuramente:
+// beforeAll(() => server.listen())
+// afterEach(() => server.resetHandlers())
+// afterAll(() => server.close())
