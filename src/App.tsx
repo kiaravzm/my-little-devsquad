cc720444fdf18d51118193fc603918f760e1a529
kiaravzm <kiaramartins.design@gmail.com>
2026-05-04 21:27:43 -0300

    chore: add app entry point and base styles

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+import { RegistrationPage } from './features/registration/components/RegistrationPage'
+
+function App() {
+
+  return (
+    <RegistrationPage />
+  )
+}
+
+export default App
