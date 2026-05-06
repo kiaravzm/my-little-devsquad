4d3f81ec6ac14807bfa7486fa7f18b86224babe4
kiaravzm <kiaramartins.design@gmail.com>
2026-05-06 20:16:35 -0300

    chore(tests): add shared test utils

---
+++ src/test/utils.tsx
@@ -0,0 +1,13 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+export const createWrapper = () => {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: { retry: false },
+      mutations: { retry: false },
+    },
+  })
+  return ({ children }) => (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  )
+}
