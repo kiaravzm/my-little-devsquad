1c971822700aab8a20a4dd481886b796f9fcff1b
kiaravzm <kiaramartins.design@gmail.com>
2026-08-04 16:00:59 -0300

    test: add jsdom polyfills for Radix Select

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,6 +1,14 @@
 import { server } from '@/services/mocks/server'
 import '@testing-library/jest-dom'
+import { vi } from 'vitest'
 
-beforeAll(() => server.listen())
+beforeAll(() => {
+  // Radix/Base UI Select no jsdom
+  HTMLElement.prototype.hasPointerCapture = vi.fn()
+  HTMLElement.prototype.setPointerCapture = vi.fn()
+  HTMLElement.prototype.releasePointerCapture = vi.fn()
+  HTMLElement.prototype.scrollIntoView = vi.fn()
+  server.listen()
+})
 afterEach(() => server.resetHandlers())
 afterAll(() => server.close())
